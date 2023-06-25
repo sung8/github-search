53eb2e1078705dc51f8c8fe698c741fb31dc57c5
sung <heechuw@gmail.com>
2023-06-25 01:23:59 -0400

    search bar button press

--- src/app/components/Search.jsx
+++ src/app/components/Search.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import { Input, Button } from '@/app/chakra'
+"use client";
+import React, { useState } from 'react';
+import { Input, Button } from '@/app/chakra';
 
 const Search = () => {
+    const [query, setQuery] = useState('')
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      alert('You searched for query');
+    }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
         <Input 
         variant={"outline"} 
         placeholder={"Type a username (i.e. sung8)"}
         focusBorderColor='green.500'
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
         />
-        <Button size="md" type='submit' colorScheme='whatsapp' mt={4}>
+        <Button size="md" type='submit' colorScheme='whatsapp' mt={4} disabled={!query} opacity={!query ? 0.5 : 1}>
             Search
         </Button>
     </form>
