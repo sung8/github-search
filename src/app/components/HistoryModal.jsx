f4c0bce5e12fd23ccad62577ad2af8fc62b5683f
sung <heechuw@gmail.com>
2023-06-27 21:07:48 -0400

    save user data on search in local storage

---
+++ src/app/components/HistoryModal.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HistoryModal = () => {
+  return <div>HistoryModal</div>;
+};
+
+export default HistoryModal;
--- src/app/components/Search.jsx
+++ src/app/components/Search.jsx
@@ -1,54 +1,77 @@
 "use client";
-import React, { useState } from 'react';
-import { Input, Button, useToast } from '@/app/chakra';
+import React, { useState } from "react";
+import { Input, Button, useToast } from "@/app/chakra";
 
-const Search = ({setUserData, setLoading}) => {
-    const [query, setQuery] = useState('')
-    const toast = useToast(); 
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-      if (!query) return;
-      setLoading(true);
-      setUserData(null);
-      try {
-        const res = await fetch(`https://api.github.com/users/${query}`);
-        const data = await res.json();
-        if (data.message) {
-          return toast({
-            title: "Error",
-            description: data.message === "Not Found" ? "User not found" : data.message,
-            status: "error",
-            duration: 3000,
-            isClosable: true
-          }) 
-        }
-        setUserData(data);
-      } catch (error) {
-        toast({
+const Search = ({ setUserData, setLoading }) => {
+  const [query, setQuery] = useState("");
+  const toast = useToast();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!query) return;
+    setLoading(true);
+    setUserData(null);
+    try {
+      const res = await fetch(`https://api.github.com/users/${query}`);
+      const data = await res.json();
+      if (data.message) {
+        return toast({
           title: "Error",
-          description: error.message,
+          description: data.message === "Not Found" ? "User not found" : data.message,
           status: "error",
           duration: 3000,
           isClosable: true
         });
-      } finally {
-        setLoading(false);
       }
-    };
+      setUserData(data);
+      addUserToLocalStorage(data, query);
+    } catch (error) {
+      toast({
+        title: "Error",
+        description: error.message,
+        status: "error",
+        duration: 3000,
+        isClosable: true
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const addUserToLocalStorage = (data, username) => {
+    const users = JSON.parse(localStorage.getItem("github-users")) || [];
+    const userExists = users.find((user) => user.id === username);
+
+    if (userExists) {
+      users.splice(users.indexOf(userExists), 1);
+    }
+    users.unshift({
+      id: username,
+      avatar_url: data.avatar_url,
+      name: data.name,
+      url: data.html_url
+    });
+  };
+
   return (
     <form onSubmit={handleSubmit}>
-        <Input 
-        variant={"outline"} 
+      <Input
+        variant={"outline"}
         placeholder={"Type a username (i.e. sung8)"}
-        focusBorderColor='green.500'
+        focusBorderColor="green.500"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        />
-        <Button size="md" type='submit' colorScheme='whatsapp' mt={4} disabled={!query} opacity={!query ? 0.5 : 1}>
-            Search
-        </Button>
+      />
+      <Button
+        size="md"
+        type="submit"
+        colorScheme="whatsapp"
+        mt={4}
+        disabled={!query}
+        opacity={!query ? 0.5 : 1}>
+        Search
+      </Button>
     </form>
-  )
-}
+  );
+};
 
 export default Search;
